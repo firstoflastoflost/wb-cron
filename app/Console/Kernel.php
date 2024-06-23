d664c7592db84f8bfd821c4cce1406aa13e9e5af
firstoflastoflost <112482375+firstoflastoflost@users.noreply.github.com>
2024-06-23 15:38:41 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,17 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateProductsPrice;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        UpdateProductsPrice::class,
+    ];
+    
     /**
      * Define the application's command schedule.
      *
